d9e01369b8b3ca0d155745ef4ee388c74579dc1d
ly <1990707901@qq.com>
2019-11-24 16:50:04 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     app
+    <el-radio v-model="radio" label="1">备选项</el-radio>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      radio: '1'
+    }
+  }
 }
 </script>
 
